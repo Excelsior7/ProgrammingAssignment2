6f99cfafb93801439123eca25d889f5b620537dc
Excelsior <anthony.biel98@gmail.com>
2022-01-16 22:44:28 +0100

    cacheMatrix.R file: added overall motivation behind the functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,20 @@
-## Put comments here that give an overall description of what your
-## functions do
+###
 
-## Write a short comment describing this function
+# The motivation behind the makeCacheMatrix function is to be able to create a self-contained
+# environment in which a matrix of interest and its inverse can be stored with the added benefit of limiting 
+# potential corruption of their values.
+
+# The motivation behind the cacheSolve function is to cache (in the closure setup) the correct inverse of 
+# the matrix cached in the closure setup passed as an argument, with the added benefit of limiting 
+# time-consuming or unnecessary computations by directly returning the inverse if it already exists.
+
+###
+
+
+# Description : makeCacheMatrix is a function whose execution environment are also a closure setup
+# for gets and sets (the closures) that interact in this environment with a matrix and its inverse.
+
+# Return value : makeCacheMatrix return a list of closures allowing to interact with the execution environment.
 
 makeCacheMatrix <- function(matrix = matrix()) {
   
@@ -14,22 +27,18 @@ makeCacheMatrix <- function(matrix = matrix()) {
   }
   
   getInverseMatrix <- function() { matrix_inverse; }
-  setInverseMatrix <- function(matrix_inverse_new) { 
-    print(sys.call(which = sys.parent(1)))
-    matrix_inverse <<- matrix_inverse_new; 
-  
-  }
+  setInverseMatrix <- function(matrix_inverse_new) { matrix_inverse <<- matrix_inverse_new; }
   
   list(getMatrix = getMatrix, setMatrix = setMatrix, 
        getInverseMatrix = getInverseMatrix, setInverseMatrix = setInverseMatrix);
 }
 
 
-# Description : cacheSolve is the interface to the closure environment passed as argument 
-# and has the function to compute and cache the inverse of the matrix cached in the closure environment
+# Description : cacheSolve is the interface to the closure setup passed as argument 
+# and has the function to compute and cache the inverse of the matrix cached in the closure setup
 # if it is not already cached in it.
 
-# Return value : The inverse of the matrix cached in the closure environment.
+# Return value : The inverse of the matrix cached in the closure setup.
 
 cacheSolve <- function(cachematrix_closures, ...) {
   
