76495a166859315ef5c0d73ca1f2373d01ce8c68
Excelsior <anthony.biel98@gmail.com>
2022-01-16 19:03:13 +0100

    cacheSolve function: initial elaboration of the function, calculates (if null), returns the inverse of the matrix.

--- cachematrix.R
+++ cachematrix.R
@@ -7,11 +7,14 @@ makeCacheMatrix <- function(matrix = matrix()) {
   
   matrix_inverse <- NULL;
   
-  getMatrix <- function() { matrix }
-  setMatrix <- function(new_matrix) { matrix <<- new_matrix }
+  getMatrix <- function() { matrix; }
+  setMatrix <- function(new_matrix) { 
+    matrix <<- new_matrix; 
+    matrix_inverse <<- NULL;
+  }
   
-  getInverseMatrix <- function() { matrix_inverse }
-  setInverseMatrix <- function(matrix_inverse_new) { matrix_inverse <<- matrix_inverse_new }
+  getInverseMatrix <- function() { matrix_inverse; }
+  setInverseMatrix <- function(matrix_inverse_new) { matrix_inverse <<- matrix_inverse_new; }
   
   list(getMatrix = getMatrix, setMatrix = setMatrix, 
        getInverseMatrix = getInverseMatrix, setInverseMatrix = setInverseMatrix);
@@ -20,6 +23,19 @@ makeCacheMatrix <- function(matrix = matrix()) {
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(cachematrix_closures, ...) {
+  
+  matrix_inverse <- cachematrix_closures$getInverseMatrix();
+  
+  if(is.null(matrix_inverse) == TRUE) {
+    
+    matrix_inverse_new <- solve(cachematrix_closures$getMatrix(), ...);
+    cachematrix_closures$setInverseMatrix(matrix_inverse_new);
+    
+    return(matrix_inverse_new);
+  }
+  
+  message("The inverse of the matrix has already been calculated and cached.");
+  matrix_inverse;
+  
 }
